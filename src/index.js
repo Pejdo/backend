b2c956dea7d2e95b3931b60a4b72a7bbc9a4b41f
Pejdo <58657684+Pejdo@users.noreply.github.com>
2021-05-03 15:28:20 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,22 +2,26 @@ import express from "express";
 const app = express();
 const port = 3000;
 app.post("/recepti", (req, res) => {
-  res.send();
+  res.json(data.recepti);
 });
 //update
 app.put("/recepti/:id", (req, res) => {});
 
-// deleting
+//
 app.delete("/recepti/:id", (req, res) => {});
 
 // jedan recept
 app.get("/recepti/:id", (req, res) => {});
 
 app.get("/", (req, res) => {});
-app.get("/kategorije", (req, res) => {});
+app.get("/kategorije", (req, res) => {
+  json(data.kategorije);
+});
 app.get("/kategorija/:id", (req, res) => {});
 app.use("/recepti", (req, res) => {});
-app.get("/korisnik", (req, res) => {});
+app.get("/korisnik", (req, res) => {
+  res.json(data.korisnik);
+});
 app.listen(port, () => {
   console.log(`slusam na portu ${port}`);
 });
